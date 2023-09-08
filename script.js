ba06ea3423812a7daf2ad41b9d97f0dd2e80742b
Rakshit Pandey <rakshit.lucky@gmail.com>
2023-09-08 13:06:35 +0530

    added logic for table

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
+// Logic for table
+const showPassword = () => {
+
+    let tb = document.querySelector("table");
+    let data = localStorage.getItem("passwords");
+    if (data == null) {
+        tb.innerHTML = "No Data To Show"
+    } else {
+        tb.innerHTML = `<tr>
+        <th>Website</th>
+        <th>Username</th>
+        <th>Password</th>
+        <th>Action</th>
+      </tr>`
+        let arr = JSON.parse(data);
+        let str = "";
+        for (let index = 0; index < arr.length; index++) {
+            const element = arr[index];
+
+            str += `<tr>
+            <td>${element.website}</td>
+            <td>${element.username}</td>
+            <td>${element.password}</td>
+            <td>${"Delete"}</td>
+        </tr> `
+        }
+        tb.innerHTML = tb.innerHTML + str;
+    }
+}
+
 console.log("Hello World");
-document.querySelector(".btn").addEventListener("click", (e)=>{
+showPassword();
+document.querySelector(".btn").addEventListener("click", (e) => {
     e.preventDefault();
     console.log("Clicked")
     console.log(username.value, password.value);
@@ -7,13 +38,14 @@ document.querySelector(".btn").addEventListener("click", (e)=>{
     console.log(passwords);
     if (passwords == null) {
         let json = [];
-        json.push({username:username.value, password:password.value})
+        json.push({ website: website.value, username: username.value, password: password.value })
         alert("Password saved");
         localStorage.setItem("passwords", JSON.stringify(json))
-    } else{
+    } else {
         let json = JSON.parse(localStorage.getItem("passwords"));
-        json.push({username:username.value, password:password.value});
+        json.push({ website: website.value, username: username.value, password: password.value });
         alert("Password Saved");
         localStorage.setItem("passwords", JSON.stringify(json));
     }
+    showPassword();
 })
